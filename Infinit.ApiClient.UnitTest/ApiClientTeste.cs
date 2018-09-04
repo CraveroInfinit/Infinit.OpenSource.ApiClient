d51d17fd44c00f6e23dbcb551094e801a653ed96
Junior Carnelos (Trabalho) <juninhocarnelos.jc@gmail.com>
2018-09-04 11:19:56 -0300

    Alter

--- Infinit.ApiClient.UnitTest/ApiClientTeste.cs
+++ Infinit.ApiClient.UnitTest/ApiClientTeste.cs
@@ -4,11 +4,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Infinit.ApiClient.UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class ApiClientTeste
     {
         [TestMethod]
         public void TestMethod1()
         {
+
+
+
+
+
         }
     }
 }
