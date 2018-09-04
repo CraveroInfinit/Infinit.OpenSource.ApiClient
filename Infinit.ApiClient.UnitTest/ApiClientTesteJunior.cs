63dd560ff341a6b1c8483798f3319e97a2de1903
Junior Carnelos (Trabalho) <juninhocarnelos.jc@gmail.com>
2018-09-04 15:45:11 -0300

    add teste junior

---
+++ Infinit.ApiClient.UnitTest/ApiClientTesteJunior.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Infinit.ApiClient.UnitTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        const string URLTest = "https://boiling-citadel-15307.herokuapp.com/";
+
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public static void ApiClientTest_TestaGetJSON_ReturnResult()
+        {
+            TestGetJSON req = new TestGetJSON
+            {
+                message = "messagem de teste"
+            };
+
+            APIClientResult result = APIClient.GetJSON(URLTest, null, typeof(TestGetJSON)).Result;
+
+
+
+
+
+
+
+
+        }
+    }
+    public class TestGetJSON
+    {
+        public string message { get; set; }
+    }
+}
